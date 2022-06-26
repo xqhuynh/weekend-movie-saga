6d0897728a4b27582b607dc2f073217ab8c663bb
Savon Huynh <xqhuynh@gmail.com>
2022-06-26 17:43:44 -0500

    Add details page route

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,17 +1,21 @@
-import {HashRouter as Router, Route} from 'react-router-dom';
+import { HashRouter as Router, Route } from 'react-router-dom';
 import './App.css';
 import MovieList from '../MovieList/MovieList'
+import DetailsPage from '../DetailsPage/DetailsPage';
 
 function App() {
   return (
     <div className="App">
       <h1>The Movies Saga!</h1>
-      <Router>        
+      <Router>
         <Route path="/" exact>
           <MovieList />
         </Route>
-        
+
         {/* Details page */}
+        <Route path="/details/:id">
+          <DetailsPage />
+        </Route>
 
         {/* Add Movie page */}
       </Router>
